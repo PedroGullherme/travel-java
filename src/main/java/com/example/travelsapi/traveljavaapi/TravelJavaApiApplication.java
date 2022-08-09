a524cec7b1ca08b30af9aae0698f371882825ba6
Pedro Gullherme <ppgrc@hotmail.com>
2022-08-09 20:41:36 -0300

    init

---
+++ src/main/java/com/example/travelsapi/traveljavaapi/TravelJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelsapi.traveljavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelJavaApiApplication.class, args);
+	}
+
+}
